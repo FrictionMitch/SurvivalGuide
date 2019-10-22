ff664551225e79a695f5cf950f1f64417ad4c8e2
DESKTOP-N4GT9CT\codeCadaver <brisson22@cogeco.ca>
2019-10-21 20:15:38 -0400

    Movement added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Tooltip("Speed of player")]
+    [SerializeField]
+    private float _speed = 10f;
+    [SerializeField]
+    private bool _isRaw = true;
+
+    private float _horizontalMovement;
+    private float _verticalMovement;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,11 +23,22 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
     }
 
     void Movement()
     {
+        if (_isRaw)
+        {
+            _horizontalMovement = Input.GetAxisRaw("Horizontal");
+            _verticalMovement = Input.GetAxisRaw("Vertical");
+        }
+        else
+        {
+            _horizontalMovement = Input.GetAxis("Horizontal");
+            _verticalMovement = Input.GetAxis("Vertical");
+        }
 
+        transform.Translate(new Vector3(_horizontalMovement, _verticalMovement, 0) * Time.deltaTime * _speed);
     }
 }
---
+++ Assets/Scripts/Powerup.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            // add score
+            Destroy(this.gameObject);
+        }
+    }
+}
